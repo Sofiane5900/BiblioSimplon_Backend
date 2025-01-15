c12490a06219c1c223589ca45cb4c9456d7dae53
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2025-01-15 16:14:17 +0100

    first commit

--- BiblioAPI/Program.cs
+++ BiblioAPI/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. // test
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -20,10 +20,7 @@ app.UseAuthorization();
 
 app.MapStaticAssets();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
