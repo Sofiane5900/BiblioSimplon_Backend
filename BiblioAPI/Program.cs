307d6a36e98f14e0f53f9b0bcd57c99137fca1b1
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2025-01-16 13:20:02 +0100

    implementation fonctionelle de swagger

--- BiblioAPI/Program.cs
+++ BiblioAPI/Program.cs
@@ -1,13 +1,40 @@
 using BiblioAPI.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container. // test
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<BiblioDbContext>(opt => opt.UseSqlite("Data Source=BiblioDb.db"));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc(
+        "v1",
+        new OpenApiInfo
+        {
+            Title = "BiblioSimplon API",
+            Version = "v1",
+            Description = "Une API pour gérer une bibliothéque de manniére numérique",
+            Contact = new OpenApiContact
+            {
+                Name = "BiblioSimplon Support",
+                Email = "BiblioSimplon@example.com",
+                Url = new Uri("https://www.BiblioSimplon.com"),
+            },
+        }
+    );
+});
 
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "BiblioSimplon API v1");
+    c.RoutePrefix = "swagger";
+});
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -26,5 +53,4 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 app.Run();
