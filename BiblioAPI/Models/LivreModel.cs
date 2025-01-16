ff456d53204b7a64f72c6f734c1370d88dd151c8
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2025-01-16 10:37:14 +0100

    renommage des models

---
+++ BiblioAPI/Models/LivreModel.cs
@@ -0,0 +1,8 @@
+﻿public class LivreModel
+{
+    public int Id { get; set; }
+    public string Titre { get; set; }
+    public string Auteur { get; set; }
+    public string ISBN { get; set; }
+    //public ICollection<Emprunter> >Emprunters { get; set; }
+}
