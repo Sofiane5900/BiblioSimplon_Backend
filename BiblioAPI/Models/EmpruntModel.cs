a1994480e72b044aca2d710c48075c62a8c95502
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2025-01-16 10:18:03 +0100

    ajout squelette empruntModel & ajustement vue

---
+++ BiblioAPI/Models/EmpruntModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BiblioAPI.Models
+{
+    public class EmpruntModel
+    {
+        public int Id { get; set; }
+
+        //public Membre Membre { get; set; }
+        //public ICollection<Livre> Livres { get; set; }
+        public DateTime DateEmprunt { get; set; }
+        public DateTime DateRetour { get; set; }
+    }
+}
